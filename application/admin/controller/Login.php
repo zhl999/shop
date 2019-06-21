84b09d03871a465b9fdb4e51046209a491071523
zhl <zhl_66@163.com>
2019-06-21 13:20:12 +0800

    login--ajax验证

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,36 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use think\captcha\Captcha;
+use Request;
+use Db;
+
+class Login extends Controller
+{
+    public function login()
+    {
+        return $this->fetch();
+    }
+
+    public function loginAction()
+    {
+        $code=Request::get('code');
+        $name=Request::get('name');
+        $pwd=Request::get('pwd');
+        $captcha = new Captcha();
+        if (!$captcha->check($code)) {
+        	$arr=['code'=>'1','status'=>'error','message'=>'验证码错误'];
+
+        }else{
+        	$where=['name'=>$name,'pwd'=>$pwd];
+        	$res=DB::table('admin_shop')->where($where)->find();
+        	if (empty($res)) {
+        		$arr=['code'=>'2','status'=>'error','message'=>'账号或密码错误'];
+        	}else{
+        		$arr=['code'=>'0','status'=>'ok','message'=>'登陆成功'];
+        	}
+        }
+        $json=json_encode($arr);
+        echo $json;
+    }
+}
